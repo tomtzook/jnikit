80b24fc5a9f81c21f50f7e81e9803829af99e1a6
tomtzook <tomtzook@gmail.com>
2022-11-07 23:44:20 +0200

    a single header to conquer them all

---
+++ include/jnikit.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <jni.h>
+
+#include "types.h"
+#include "signatures.h"
+#include "except.h"
+#include "class.h"
+#include "array.h"
+#include "objects.h"
+#include "env.h"
+#include "exceptions.h"
+#include "context.h"
