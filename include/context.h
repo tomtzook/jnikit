b79c7d83b74170091d77be6dc49ebe66d7071d68
tomtzook <tomtzook@gmail.com>
2022-11-10 23:09:33 +0200

    void specialization of context

--- include/context.h
+++ include/context.h
@@ -18,25 +18,21 @@ template<typename R>
 R context(JNIEnv* env, std::function<R(Env&)> func) {
     try {
         Env jnikitEnv(env);
-        return func(jnikitEnv);
+        if constexpr (std::is_void_v<R>) {
+            func(jnikitEnv);
+        } else {
+            return func(jnikitEnv);
+        }
     } catch (const PendingJavaException&) {
-        if constexpr (std::is_integral_v<R>) {
+        if constexpr (std::is_void_v<R>) {
+            return;
+        } else if constexpr (std::is_integral_v<R>) {
             return 0;
         } else if constexpr (std::is_pointer_v<R>) {
             return nullptr;
         } else {
             static_assert(meta::always_false<R>);
         }
-    }
-}
-
-template<>
-void context(JNIEnv* env, std::function<void(Env&)> func) {
-    try {
-        Env jnikitEnv(env);
-        func(jnikitEnv);
-    } catch (const PendingJavaException&) {
-        return;
     }
 }
 
