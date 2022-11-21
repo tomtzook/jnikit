fc689df16732fdf88cd89b8d88a227fe7719a539
tomtzook <tomtzook@gmail.com>
2022-11-21 20:54:11 +0200

    object wrap

---
+++ build/CMakeFiles/jnikit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jnikit.dir/src/empty.cpp.o"
+  "CMakeFiles/jnikit.dir/src/except.cpp.o"
+  "libjnikit.a"
+  "libjnikit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jnikit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
